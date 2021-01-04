1ab93d4f108ba010a57ea5035f0f983ffa06c359
barosanuemailtest <barosanuemailtest@gmail.com>
2021-01-04 15:36:13 +0200

    put to dynamodb

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -1,14 +1,23 @@
+import { v4 as uuid } from 'uuid';
+import AWS from 'aws-sdk';
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
 async function createAuction(event, context) {
   const title = JSON.parse(event.body);
   const now = new Date().toISOString();
 
   const auction = {
+    id: uuid(),
     title: title,
     status: 'OPEN',
     createdAt: now
-  }
+  };
 
-
+  await dynamodb.put({
+    TableName: "AuctionsTable",
+    Item: auction
+  }).promise();
 
   return {
     statusCode: 200,
