8178fd2ab47a23ecdfe5ff8f64c568ca8b80fb27
barosanuemailtest <barosanuemailtest@gmail.com>
2021-01-05 17:09:58 +0200

    Place bid check

--- src/handlers/placeBid.js
+++ src/handlers/placeBid.js
@@ -1,12 +1,20 @@
 import AWS from 'aws-sdk';
 import commonMiddleware from '../lib/commonMidlleware';
 import createError from "http-errors";
+import { getAuctionById } from "../handlers/getAuction"
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
 async function placeBId(event, context) {
     const { id } = event.pathParameters
     const { amount } = event.body;
+
+    const auction = await getAuctionById(id);
+
+    if (amount <=auction.highestBid.amount) {
+        throw new createError.Forbidden(`Your bid must be higher than ${auction.highestBid.amount}!`)
+    }
+
     const params = {
         TableName: process.env.AUCTIONS_TABLE_NAME,
         Key: { id },
