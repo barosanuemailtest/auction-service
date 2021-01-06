8eddacccd0c51e50e94c924d6843036319ca2b14
barosanuemailtest <barosanuemailtest@gmail.com>
2021-01-06 11:33:40 +0200

    get auctions query

--- src/handlers/getAuctions.js
+++ src/handlers/getAuctions.js
@@ -5,16 +5,28 @@ import createError from "http-errors";
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
 async function getAuctions(event, context) {
+    const { status } = event.queryStringParameters;
     let auctions;
-
+  
+    const params = {
+      TableName: process.env.AUCTIONS_TABLE_NAME,
+      IndexName: 'statusAndEndDate',
+      KeyConditionExpression: '#status = :status',
+      ExpressionAttributeValues: {
+        ':status': status,
+      },
+      ExpressionAttributeNames: {
+        '#status': 'status',
+      },
+    };
+  
     try {
-        const result = await dynamodb.scan({
-            TableName: process.env.AUCTIONS_TABLE_NAME
-        }).promise();
-        auctions = result.Items
+      const result = await dynamodb.query(params).promise();
+  
+      auctions = result.Items;
     } catch (error) {
-        console.error(error);
-        throw new createError.InternalServerError(error);
+      console.error(error);
+      throw new createError.InternalServerError(error);
     }
 
 
